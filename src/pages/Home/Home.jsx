72c50261cbb54755a5c2c89d803566496e4ecdf1
Jessica Adzoyi <121783593+JESSICA047@users.noreply.github.com>
2025-02-20 11:27:09 +0000

    vercel please work now

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Hero from "../../components/Hero/Hero";
 import Nutrition from "../../components/Nutrition/Nutrition";
-import "./home.css"
+import "./Home.css"
 
 function Home() {
   return (
